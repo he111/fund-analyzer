0628bbbe318c6c813c0b51e7d4965cc6a0534000
Yuying He <my@personal>
2020-06-29 01:52:23 -0400

    consumed API

--- src/components/HighchartsComponent.js
+++ src/components/HighchartsComponent.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import "../../src/App.css";
 import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import {
   Container,
   Row,
@@ -16,105 +17,123 @@ import {
   FormControl,
 } from "react-bootstrap";
 
-const options = {
-  chart: {
-    type: "column",
-    width: 800,
-  },
-  title: {
-    text: "Potential Cost Over Time",
-    align: "left",
-  },
-  series: [
-    {
-      name: "Annual Operating Expense",
-      data: [
-        0,
-        0.1,
-        0.32,
-        0.33,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-        0.45,
-      ],
-    },
-    { name: "Front-End Sales Charges" },
-    { name: "Back-End Sales Charges" },
-    { name: "Potential Redemption Fees" },
-  ],
-  xAxis: {
-    allowDecimals: false,
-    title: {
-      text: "Year",
-    },
-    categories: [
-      "0",
-      "0-1",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "12",
-      "14",
-      "16",
-      "18",
-      "20",
-    ],
-  },
-  yAxis: {
-    min: 0,
-    title: {
-      text: null,
-    },
-    labels: {
-      format: "{value}%",
-      // step:2.5,
-    },
-    max: 10,
-  },
-  legend: {
-    layout: "vertical",
-    align: "right",
-    verticalAlign: "top",
-    x: -40,
-    y: 80,
-    floating: true,
-    borderWidth: 1,
-    backgroundColor:
-      Highcharts.defaultOptions.legend.backgroundColor || "#FFFFFF",
-    shadow: true,
-  },
-  tooltip: {
-    formatter: function () {
-      return (
-        "Year: " +
-        this.point.x +
-        "<br/>Annual Operating Expenses:" +
-        this.point.y +
-        "%"
-      );
-    },
-  },
-};
+
 function HighChartsComponent() {
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+    const fetchData = async () => {
+        await axios.get(
+          `https://my-json-server.typicode.com/he111/simple-JSON-server/profile`
+        )
+        .then((res) => {
+          const datum = res.data;
+          console.log(datum[0].data);
+          this.setState({ data: datum[0].data });
+          console.log(this.state.data);
+        });
+    }
+    fetchData();
+    },[]);
+    const options = {
+      chart: {
+        type: "column",
+        width: 800,
+      },
+      title: {
+        text: "Potential Cost Over Time",
+        align: "left",
+      },
+      series: [
+        {
+          name: "Annual Operating Expense",
+          data: this.state.data,
+          //   [
+          //     0,
+          //     0.1,
+          //     0.32,
+          //     0.33,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //     0.45,
+          //   ],
+        },
+        { name: "Front-End Sales Charges" },
+        { name: "Back-End Sales Charges" },
+        { name: "Potential Redemption Fees" },
+      ],
+      xAxis: {
+        allowDecimals: false,
+        title: {
+          text: "Year",
+        },
+        categories: [
+          "0",
+          "0-1",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "12",
+          "14",
+          "16",
+          "18",
+          "20",
+        ],
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: null,
+        },
+        labels: {
+          format: "{value}%",
+          // step:2.5,
+        },
+        max: 10,
+      },
+      legend: {
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "top",
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor:
+          Highcharts.defaultOptions.legend.backgroundColor || "#FFFFFF",
+        shadow: true,
+      },
+      tooltip: {
+        formatter: function () {
+          return (
+            "Year: " +
+            this.point.x +
+            "<br/>Annual Operating Expenses:" +
+            this.point.y +
+            "%"
+          );
+        },
+      },
+    };
+    
     return (
-      <div id="highcharts">
+      <div id="highcharts" >
         <HighchartsReact highcharts={Highcharts} options={options} />
         <div className="warning">
           <FontAwesomeIcon
